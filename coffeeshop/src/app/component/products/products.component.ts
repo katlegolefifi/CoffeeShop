289ce4b469b90ffd98a4b16511cbfd5c0c27adb8
Katlego <katlegolefifi048@gmail.com>
2023-03-17 15:05:13 +0200

    json server added

--- coffeeshop/src/app/component/products/products.component.ts
+++ coffeeshop/src/app/component/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/api/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+public productist:any;
+  constructor(private api:ProductsService) { }
 
   ngOnInit(): void {
+    this.api.getproduct().subscribe(res=>{
+      //console.log(res);
+      this.productist = res;
+    })
   }
 
 }
